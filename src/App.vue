fa5f31875437c5b06ed19c6b056e90740439f2bc
Tom <tramin@et.esiea.fr>
2019-10-10 15:33:42 +0200

    couleur rouge

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar dark style="background-color:red" app>
+    <v-app-bar dark style="background-color:#930000" app>
       <v-toolbar-title  class="headline text-uppercase">
         <span>projet web 4afsi2 ignite</span></v-toolbar-title>
       <v-spacer></v-spacer>
