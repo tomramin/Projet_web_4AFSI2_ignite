f4ddab949da5595c3b38dfb75c0c039a6e608d10
Tom <tramin@et.esiea.fr>
2019-10-15 14:16:18 +0200

    si connecté passer à la suite

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,39 +1,44 @@
 <template>
   <article>
-    <div class="container" :class="{'sign-up-active' : signUp}">
-      <div class="overlay-container">
-        <div class="overlay">
-          <div class="overlay-left">
-            <h2>Welcome Back!</h2>
-            <p>Please login with your personal info</p>
-            <button class="invert" id="signIn" @click="signUp = !signUp">Sign In</button>
+    <v-content>
+      <v-container v-if="!connected">
+        <div class="container" :class="{'sign-up-active' : signUp}">
+          <div class="overlay-container">
+            <div class="overlay">
+              <div class="overlay-left">
+                <h2>Welcome Back!</h2>
+                <p>Please login with your personal info</p>
+                <button class="invert" id="signIn" @click="signUp = !signUp">Sign In</button>
+              </div>
+              <div class="overlay-right">
+                <h2>Hello, Friend!</h2>
+                <p>Please enter your personal details</p>
+                <button class="invert" id="signUp" @click="signUp = !signUp">Sign Up</button>
+              </div>
+            </div>
           </div>
-          <div class="overlay-right">
-            <h2>Hello, Friend!</h2>
-            <p>Please enter your personal details</p>
-            <button class="invert" id="signUp" @click="signUp = !signUp">Sign Up</button>
-          </div>
+          <form class="sign-up" action="#">
+            <h2>Create login</h2>
+            <input v-model="name" placeholder="Name" />
+            <input v-model="password" type="password" placeholder="Password" />
+            <button @click="inscription">Sign Up</button>
+          </form>
+          <form class="sign-in" action="#">
+            <h2>Sign In</h2>
+            <div>Use your account</div>
+            <input v-model="name" placeholder="Name" />
+            <input v-model="password" type="password" placeholder="Password" />
+            <a href="#">Forgot your password?</a>
+            <button @click="login">Sign In</button>
+          </form>
         </div>
-      </div>
-      <form class="sign-up" action="#">
-        <h2>Create login</h2>
-        <input v-model="name" placeholder="Name" />
-        <input v-model="password" type="password" placeholder="Password" />
-        <button @click="inscription">Sign Up</button>
-      </form>
-      <form class="sign-in" action="#">
-        <h2>Sign In</h2>
-        <div>Use your account</div>
-        <input v-model="name" placeholder="Name" />
-        <input v-model="password" type="password" placeholder="Password" />
-        <a href="#">Forgot your password?</a>
-        <button @click="login">Sign In</button>
-      </form>
-    </div> <v-content>
-    <v-container text-center v-if="connected">
+      </v-container>
+    </v-content>
+    <v-content>
+      <v-container text-center v-if="connected">
         <p>Bienvenue {{name}}</p>
-    </v-container>
-  </v-content>
+      </v-container>
+    </v-content>
   </article>
 </template>
 
